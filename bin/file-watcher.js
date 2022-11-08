ee39f1a5d69039f25591758aa87aa38a93fe8009
TTA21 <lucasmateusgoncalves20@gmail.com>
2022-11-08 13:43:56 -0300

    string parsing to json_decode fix

--- bin/file-watcher.js
+++ bin/file-watcher.js
@@ -7,8 +7,8 @@ const params = JSON.parse(process.argv[3]);
 const watcher = chokidar.watch(paths, params);
 
 watcher
-    .on('add', (path, stat) => console.log( JSON.stringify( { type: 'fileCreated', path: path, stat: stat } ) ))
-    .on('change', (path, stat) => console.log( JSON.stringify( { type: 'fileUpdated', path: path, stat: stat } ) ))
-    .on('unlink', (path) => console.log( JSON.stringify( { type: 'fileDeleted', path: path, stat: [] } ) ))
-    .on('addDir', (path, stat) => console.log( JSON.stringify( { type: 'directoryCreated', path: path, stat: stat } ) ))
-    .on('unlinkDir', (path) => console.log( JSON.stringify( { type: 'directoryDeleted', path: path, stat: [] } ) ))
+    .on('add', (path, stat) => console.log( JSON.stringify( { type: 'fileCreated', path: path, stats: stat } ) ))
+    .on('change', (path, stat) => console.log( JSON.stringify( { type: 'fileUpdated', path: path, stats: stat } ) ))
+    .on('unlink', (path) => console.log( JSON.stringify( { type: 'fileDeleted', path: path, stats: [] } ) ))
+    .on('addDir', (path, stat) => console.log( JSON.stringify( { type: 'directoryCreated', path: path, stats: stat } ) ))
+    .on('unlinkDir', (path) => console.log( JSON.stringify( { type: 'directoryDeleted', path: path, stats: [] } ) ))
